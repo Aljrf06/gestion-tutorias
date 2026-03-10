43421548da8deb2262b572b371380923fb75f7b7
Aljrf06 <138081741+Aljrf06@users.noreply.github.com>
2026-03-10 16:26:58 -0500

    se agrega el proyecto inicial

---
+++ src/main/java/co/edu/sistemagestiontutoria/SistemaGestionTutoriaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sistemagestiontutoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionTutoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaGestionTutoriaApplication.class, args);
+    }
+
+}
